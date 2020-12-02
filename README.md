2e25847412b244b21a1d4273673b7b9b428dc105
Nathan <nathanpykimutai@gmail.com>
2020-12-03 00:40:03 +0300

    connected to the WiFi and now starting a WifiServer

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # remoteswitch
 Swtching on and off leds remotely(over the internet) Just like a smart home
+
+## Setup
+### Requirements
+* Arduino IDE
+* esp8266 board
+
+### Setup the arduino ide to support esp8266 mcu
+Install Arduino IDE
+
+
+
+
+# Contribution
+I don't actively support this project so you might want to reduce your pull
+requests 
+# License
+MIT
--- remoteswitch.ino
+++ remoteswitch.ino
@@ -1,7 +1,26 @@
+#include <ESP8266WiFi.h>
+
+const char* ssid = "MUGO"; //replace this with your own ssid
+const char* password = "mugo2020"; //replace this with your password
+
+int ledPin = 13;
+
+
 void setup() {
   Serial.begin(115200);
+  delay(10);
+  pinMode(ledPin,OUTPUT);
+  digitalWrite(ledPing,LOW);
+
+  //connect to wifi
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly
+  digitalWrite(13,HIGH);
+  delay(1000);
+  digitalWrite(13,LOW);
+  delay(100);
 }
